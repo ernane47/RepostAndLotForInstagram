ceec53f321ab1bb7e8bfa814ab6a1d1dc063c1b2
Ernane <josiel.ernand_2@hotmail.com>
2017-06-30 15:37:24 -0300

    enviando;

--- app/src/main/java/com/example/pc/repostandlotforinstagram/MainActivity.java
+++ app/src/main/java/com/example/pc/repostandlotforinstagram/MainActivity.java
@@ -83,7 +83,7 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
-
+            
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
